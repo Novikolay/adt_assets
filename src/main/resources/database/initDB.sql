c90fd89008797ece390d57258d27d26ec5ee9d2e
Novikolay <nka733@yandex.ru>
2020-05-26 00:28:28 +0300

    change data to using in IDEA EAP

--- src/main/resources/database/initDB.sql
+++ src/main/resources/database/initDB.sql
@@ -1,7 +1,17 @@
+-- CREATE TABLE IF NOT EXISTS clients
+-- (
+--     id    BIGSERIAL PRIMARY KEY ,
+--     name  VARCHAR(200) NOT NULL ,
+--     email VARCHAR(254) NOT NULL ,
+--     phone VARCHAR(20)  NOT NULL
+-- );
+--
+--DROP TABLE IF EXISTS clients
+--
 CREATE TABLE IF NOT EXISTS clients
 (
-    id    BIGSERIAL PRIMARY KEY ,
+    id    SERIAL PRIMARY KEY ,
     name  VARCHAR(200) NOT NULL ,
     email VARCHAR(254) NOT NULL ,
-    phone VARCHAR(20)  NOT NULL
+    phone VARCHAR(50)  NOT NULL
 );
