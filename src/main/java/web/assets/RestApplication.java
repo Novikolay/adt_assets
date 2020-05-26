1161c7e6d460f548422e701df3dac985bb12eda1
Novikolay <nka733@yandex.ru>
2020-05-27 01:51:37 +0300

    add authorization | Not stable

---
+++ src/main/java/web/assets/RestApplication.java
@@ -0,0 +1,35 @@
+package web.assets;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import web.assets.repository.FileStorage;
+
+import javax.annotation.Resource;
+
+@SpringBootApplication
+public class RestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApplication.class, args);
+    }
+
+}
+
+
+//@SpringBootApplication
+//public class RestApplication implements CommandLineRunner {
+//
+//    @Resource
+//    FileStorage fileStorage;
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(RestApplication.class, args);
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        fileStorage.deleteAll();
+//        fileStorage.init();
+//    }
+//}
