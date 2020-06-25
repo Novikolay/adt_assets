cadca898a4d23f181776209145234f59d1faaaee
Nikolay Novikov <nnovikov@wildred.ru>
2020-06-25 16:40:19 +0300

    temp | docker work, but DB not migrate

---
+++ src/main/resources/db/migration/V0_0_00__initDB.sql
@@ -0,0 +1,12 @@
+-- DROP TABLE IF EXISTS banners;
+-- DROP SEQUENCE IF EXISTS banners_id_seq;
+CREATE TABLE IF NOT EXISTS banners
+(
+    id     INTEGER PRIMARY KEY ,
+    name   VARCHAR(200) NOT NULL ,
+    path   VARCHAR(400) NOT NULL ,
+    type   VARCHAR(20) NOT NULL ,
+    status BOOLEAN ,
+    drm    VARCHAR(10) NOT NULL
+);
+CREATE SEQUENCE banners_id_seq START WITH 25 INCREMENT BY 1;
